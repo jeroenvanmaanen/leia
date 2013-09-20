1c5355a55e5aa337eb4a0a0da710dbeab4ad0362
Jeroen van Maanen <jeroen@techtribe.nl>
2013-09-20 23:33:56 +0200

    First graph test.

---
+++ src/test/java/org/leialearns/graph/GraphTest.java
@@ -0,0 +1,113 @@
+package org.leialearns.graph;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.leialearns.utilities.ExecutionListener;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.test.TestGraphDatabaseFactory;
+import org.neo4j.tooling.GlobalGraphOperations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.leialearns.utilities.Static.getLoggingClass;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/org/leialearns/AppTest-context.xml"})
+@TestExecutionListeners(value = {DependencyInjectionTestExecutionListener.class, ExecutionListener.class})
+public class GraphTest {
+    private final Logger logger = LoggerFactory.getLogger(getLoggingClass(this));
+    private static GraphDatabaseService db;
+    private static ExecutionEngine executionEngine;
+
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        db = createDatabase();
+        executionEngine = new ExecutionEngine(db);
+    }
+
+    @AfterClass
+    public static void afterClass() throws IOException {
+        db.shutdown();
+    }
+
+    @Test
+    public void testGraph() {
+        // when
+        ExecutionResult results = distance("Ben", "Mike");
+        // then
+        assertTrue( results.iterator().hasNext() );
+        assertEquals( 4, results.iterator().next().get( "distance" ) );
+    }
+
+    public ExecutionResult distance( String firstUser, String secondUser )
+    {
+        String query = "START first=node:user({firstUserQuery}),\n" +
+                " second=node:user({secondUserQuery})\n" +
+                "MATCH p=shortestPath(first-[*..4]-second)\n" +
+                "RETURN length(p) AS distance";
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put( "firstUserQuery", "name:" + firstUser );
+        params.put( "secondUserQuery", "name:" + secondUser );
+        return executionEngine.execute( query, params );
+    }
+
+    public static GraphDatabaseService createDatabase() {
+        // Create nodes
+        String cypher = "CREATE\n" +
+                "(ben {name:'Ben', _label:'user'}),\n" +
+                "(arnold {name:'Arnold', _label:'user'}),\n" +
+                "(charlie {name:'Charlie', _label:'user'}),\n" +
+                "(gordon {name:'Gordon', _label:'user'}),\n" +
+                "(lucy {name:'Lucy', _label:'user'}),\n" +
+                "(emily {name:'Emily', _label:'user'}),\n" +
+                "(sarah {name:'Sarah', _label:'user'}),\n" +
+                "(kate {name:'Kate', _label:'user'}),\n" +
+                "(mike {name:'Mike', _label:'user'}),\n" +
+                "(paula {name:'Paula', _label:'user'}),\n" +
+                "ben-[:FRIEND]->charlie,\n" +
+                "charlie-[:FRIEND]->lucy,\n" +
+                "lucy-[:FRIEND]->sarah,\n" +
+                "sarah-[:FRIEND]->mike,\n" +
+                "arnold-[:FRIEND]->gordon,\n" +
+                "gordon-[:FRIEND]->emily,\n" +
+                "emily-[:FRIEND]->kate,\n" +
+                "kate-[:FRIEND]->paula";
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ExecutionEngine engine = new ExecutionEngine( db );
+        engine.execute( cypher );
+
+        // Index all nodes in "user" index
+        Transaction tx = db.beginTx();
+        try {
+            Iterable<Node> allNodes =
+                    GlobalGraphOperations.at(db).getAllNodes();
+            for ( Node node : allNodes ) {
+                if ( node.hasProperty( "name" ) )
+                {
+                    db.index().forNodes( "user" )
+                            .add( node, "name", node.getProperty( "name" ) );
+                }
+            }
+            tx.success();
+        } finally {
+            tx.finish();
+        }
+        return db;
+    }
+}
