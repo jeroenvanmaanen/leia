0bb298467dccbee059fb980b23c9553f85308dd3
Jeroen van Maanen <jeroen@sollunae.net>
2013-11-27 06:25:33 +0100

    Improved implementation of "Expected...".

--- src/main/java/org/leialearns/graph/model/EstimateDAO.java
+++ src/main/java/org/leialearns/graph/model/EstimateDAO.java
@@ -1,49 +1,121 @@
 package org.leialearns.graph.model;
 
+import org.leialearns.enumerations.ModelType;
 import org.leialearns.graph.IdDaoSupport;
 import org.leialearns.graph.interaction.SymbolDTO;
 import org.leialearns.graph.structure.NodeDTO;
 import org.leialearns.utilities.TypedIterable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
 
-public class EstimateDAO extends IdDaoSupport<EstimateDTO> {
+import static org.leialearns.utilities.Static.getLoggingClass;
 
-    @Autowired
+public class EstimateDAO extends IdDaoSupport<EstimateDTO> {
+    private final Logger logger = LoggerFactory.getLogger(getLoggingClass(this));
+
     EstimateRepository repository;
 
+    @Autowired
+    ToggledRepository toggledRepository;
+
+    @Autowired
+    FractionDAO fractionDAO;
+
+    @Autowired
+    public EstimateDAO(EstimateRepository repository) {
+        super(repository);
+        this.repository = repository;
+    }
+
     public EstimateDTO findEstimate(VersionDTO version, NodeDTO node, SymbolDTO symbol) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        return repository.findEstimate(version, node, symbol);
     }
 
     public TypedIterable<EstimateDTO> findEstimates(VersionDTO version, NodeDTO node) {
         Set<EstimateDTO> result = repository.findEstimatesByVersionAndNode(version, node);
-        return new TypedIterable<EstimateDTO>(result, EstimateDTO.class);
+        return new TypedIterable<>(result, EstimateDTO.class);
     }
 
     public TypedIterable<EstimateDTO> findEstimates(VersionDTO version) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        Set<EstimateDTO> estimates = repository.findEstimates(version);
+        return new TypedIterable<>(estimates, EstimateDTO.class);
     }
 
     public EstimateDTO createEstimate(ExpectedDTO expected, NodeDTO node, SymbolDTO symbol, FractionBaseDTO fraction) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        return findOrCreate(expected.getVersion(), node, symbol, fraction);
     }
 
     public EstimateDTO createEstimate(ToggledDTO toggled, NodeDTO node, SymbolDTO symbol, FractionBaseDTO fraction) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        return findOrCreate(toggled.getVersion(), node, symbol, fraction);
     }
 
     public EstimateDTO findOrCreate(VersionDTO version, NodeDTO node, SymbolDTO symbol, FractionBaseDTO fraction) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        EstimateDTO result = findEstimate(version, node, symbol);
+        if (result == null) {
+            create(version, node, symbol, fraction);
+        } else {
+            if (logger.isTraceEnabled()) {
+                String fractionString = String.valueOf(fraction);
+                logger.trace("Existing persistent fraction for: " + version + ": " + node + ": " + symbol + " -> " + fractionString);
+            }
+            if (!fraction.equals(result.getFraction())) {
+                throw new IllegalStateException("Existing estimate does not match fraction: " + result + ": " + fraction);
+            }
+        }
+        return result;
     }
 
     public EstimateDTO create(VersionDTO version, NodeDTO node, SymbolDTO symbol, FractionBaseDTO fraction) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        FractionEstimateDTO fractionEstimate;
+        if (fraction instanceof FractionEstimateDTO) {
+            fractionEstimate = (FractionEstimateDTO) fraction;
+        } else {
+            fractionEstimate = (FractionEstimateDTO) fractionDAO.findOrCreateFraction(null, -1, fraction.getNumerator(), fraction.getDenominator());
+        }
+        logger.trace("Create estimate for: " + version + ": " + node + ": " + symbol + ": " + fraction);
+        EstimateDTO estimate = new EstimateDTO();
+        estimate.setVersion(version);
+        estimate.setNode(node);
+        estimate.setSymbol(symbol);
+        estimate.setFraction(fractionEstimate);
+        estimate = save(estimate);
+        logger.trace("Created estimate: " + estimate);
+        return estimate;
     }
 
     public void copyEstimates(final ExpectedDTO expected, final VersionDTO sourceVersion) {
-        throw new UnsupportedOperationException("TODO: implement"); // TODO: implement
+        VersionDTO targetVersion = expected.getVersion();
+        logger.debug("Copy estimates: {} => {}", sourceVersion, targetVersion);
+        Set<NodeDTO> nodes = repository.findEstimateNodes(sourceVersion);
+        logger.trace("Nodes: {");
+        for (NodeDTO node : nodes) {
+            logger.trace("  Node: {}", node);
+            Integer affected = repository.deleteEstimates(targetVersion, node);
+            logger.debug("Deleted: {} estimate nodes", affected);
+        }
+        logger.trace("}");
+
+        if (sourceVersion.getModelType() == ModelType.TOGGLED) {
+            ToggledDTO toggled = toggledRepository.findByVersion(sourceVersion);
+            logger.debug("Toggled: {}", toggled);
+            if (!toggled.getInclude()) {
+                Integer affected = repository.deleteEstimates(targetVersion, toggled.getNode());
+                logger.debug("Deleted: {} estimate nodes", affected);
+            }
+        }
+
+        int count = 0;
+        try {
+            for (EstimateDTO estimate : findEstimates(sourceVersion)) {
+                create(targetVersion, estimate.getNode(), estimate.getSymbol(), estimate.getFraction());
+                count++;
+            }
+        } finally {
+            logger.debug("Copied estimates: " + count);
+        }
     }
 
 }
--- src/main/java/org/leialearns/graph/model/EstimateRepository.java
+++ src/main/java/org/leialearns/graph/model/EstimateRepository.java
@@ -1,10 +1,34 @@
 package org.leialearns.graph.model;
 
+import org.leialearns.graph.interaction.SymbolDTO;
 import org.leialearns.graph.structure.NodeDTO;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 
 import java.util.Set;
 
 public interface EstimateRepository extends GraphRepository<EstimateDTO> {
     Set<EstimateDTO> findEstimatesByVersionAndNode(VersionDTO version, NodeDTO node);
+
+    @Query("START version = node({0}), node = node({1}), symbol = node({2})" +
+            " MATCH version<-[:FOR_VERSION]-estimate-[:FOR_NODE]->node, estimate-[:FOR_SYMBOL]->symbol" +
+            " RETURN estimate")
+    EstimateDTO findEstimate(VersionDTO version, NodeDTO nodeDTO, SymbolDTO symbol);
+
+    @Query("START version = node({0})" +
+            " MATCH version<-[:FOR_VERSION]-estimate" +
+            " RETURN estimate")
+    Set<EstimateDTO> findEstimates(VersionDTO version);
+
+    @Query("START version = node({0})" +
+            " MATCH version<-[:FOR_VERSION]-estimate-[:FOR_NODE]->node" +
+            " RETURN distinct node")
+    Set<NodeDTO> findEstimateNodes(VersionDTO version);
+
+    @Query("START version = node({0}), node = node({1})" +
+            " MATCH version<-[:FOR_VERSION]-estimate-[:FOR_NODE]->node" +
+            " DELETE estimate" +
+            " RETURN count(estimate)")
+    Integer deleteEstimates(VersionDTO version, NodeDTO node);
+
 }
